2c435dff69f0f2c9eb573fa687bdc52c34546b52
Kamit6337 <kamit6337@gmail.com>
2024-03-19 10:40:59 +0530

    cors url changed

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ["https://notable-client.vercel.app"],
+  origin: ["https://notable-client.onrender.com"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
