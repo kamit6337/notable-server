da798c766cb05a03daab3b017b66b2a61231811a
Kamit6337 <kamit6337@gmail.com>
2024-03-22 10:57:44 +0530

    update

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,7 +1,7 @@
 import { environment } from "./environment.js";
 
 export const corsOptions = {
-  origin: ["https://www.notable-client.onrender.com"],
+  origin: ["https://notable-client.onrender.com"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
