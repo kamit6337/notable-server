190d94674087ac19b33aa7df675beebe0c958ff6
Kamit6337 <kamit6337@gmail.com>
2024-04-13 16:16:06 +0530

    update cors

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,13 +1,5 @@
-const allowedUrlList = ["https://notable-client.onrender.com"];
-
 export const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedUrlList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: ["https://notable-client.onrender.com"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
