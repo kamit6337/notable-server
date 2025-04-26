f5bb4da099ff78e5d3c8e551d0e1ad29d9f40df0
Kamit6337 <kamit6337@gmail.com>
2025-04-26 13:19:06 +0530

    update createNotebook for unique name, update createNote for unique title

--- databases/notebooks/createNewNotebook.js
+++ databases/notebooks/createNewNotebook.js
@@ -2,6 +2,15 @@ import { Notebook } from "../../models/NotebookModel.js";
 import { setNewNotebookIntoRedis } from "../../redis/notebooks/notebooksFromRedis.js";
 
 const createNewNotebook = async (userId, name) => {
+  const checkAlreadyExistWithName = await Notebook.exists({
+    user: userId,
+    title: name,
+  });
+
+  if (!!checkAlreadyExistWithName) {
+    throw new Error("Notebook name must be unique. Try different name");
+  }
+
   const newNotebook = await Notebook.create({
     user: userId,
     title: name,
--- databases/notes/createNoteDB.js
+++ databases/notes/createNoteDB.js
@@ -2,14 +2,39 @@ import { Note } from "../../models/NoteModel.js";
 import { setNewNoteIntoRedis } from "../../redis/notes/notesFromRedis.js";
 
 const createNoteDB = async (userId, obj) => {
-  const note = await Note.create(obj);
+  const findListOfUnitiledNoNotes = await Note.find({
+    user: userId,
+    title: new RegExp("Untitled", "i"),
+  }).lean();
 
-  console.log("note is created", note);
+  let usedNumbers = new Set();
+
+  if (findListOfUnitiledNoNotes?.length > 0) {
+    findListOfUnitiledNoNotes.forEach((note) => {
+      if (note.title === "Untitled") {
+        usedNumbers.add(0);
+      } else {
+        const count = note.title.split("-")[1];
+        usedNumbers.add(parseInt(count));
+      }
+    });
+  }
+
+  // Find the smallest available number
+  let availableNumber = 0;
+  while (usedNumbers.has(availableNumber)) {
+    availableNumber++;
+  }
+
+  const title =
+    availableNumber === 0 ? "Untitled" : `Untitled-${availableNumber}`;
+
+  const note = await Note.create({
+    ...obj,
+    title,
+  });
 
   await setNewNoteIntoRedis(userId, note);
-
-  console.log("note is send");
-
   return note;
 };
 
