70995407966653d5344637779237227c234a0d42
Kamit6337 <kamit6337@gmail.com>
2025-04-26 14:09:51 +0530

    minor update

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,5 +1,9 @@
 export const corsOptions = {
-  origin: ["https://notable-client.onrender.com", "http://localhost:5173"],
+  origin: [
+    "https://notable-client.onrender.com",
+    "https://notable-client-8u6c.onrender.com",
+    "http://localhost:5173",
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
